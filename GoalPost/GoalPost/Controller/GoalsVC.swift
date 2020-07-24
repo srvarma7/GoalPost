41ab24a73bcb20da5780ec37d1842cede6d0293b
srvarma7 <srvarma7@gmail.com>
2020-07-24 15:04:29 +1000

    Adding tableview and create button action

---
+++ GoalPost/GoalPost/Controller/GoalsVC.swift
@@ -0,0 +1,35 @@
+//
+//  GoalsVC.swift
+//  GoalPost
+//
+//  Created by Sai Raghu Varma Kallepalli on 24/7/20.
+//  Copyright © 2020 Sai Raghu Varma Kallepalli. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    // MARK: - Goal button tapped
+    @IBAction func onAddingGoalTapped(_ sender: Any) {
+    }
+    
+    /*
+    
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
